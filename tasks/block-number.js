d355e851edcf6f862ceca3419ff3fcaa70a0f797
0xmohitsen <mohitsen0102@gmail.com>
2024-10-13 07:50:30 +0530

    Added a task

--- tasks/block-number.js
+++ tasks/block-number.js
@@ -2,8 +2,8 @@ const { task } = require("hardhat/config");
 
 task("block-number", "Prints the current block number").setAction(
     async (taskArgs, hre) => {
-        const provider = hre.ethers.provider; // Get the provider instance
-        const blockNumber = await provider.getBlockNumber(); // Use the provider to get the block number
+        const provider = hre.ethers.provider;
+        const blockNumber = await provider.getBlockNumber();
         console.log(`Current Block Number: ${blockNumber}`);
     },
 );
